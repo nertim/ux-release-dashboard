448c8c742dcbd01994931f5910f23a959eb984ab
Travis Harris <travih@microsoft.com>
2019-02-02 15:19:55 -0800

    move up in the world

--- version-scraper/IbizaScraper/index.js
+++ version-scraper/IbizaScraper/index.js
@@ -1,68 +1,114 @@
-const axios = require("axios");
-const MongoClient = require("mongodb").MongoClient;
-const url =
-  process.env.MONGO_DB_CONNECTION_STRING;
-module.exports = async function(context, myTimer) {
-  const ibizaVersionsUri =
-    "https://appsvcstorage.blob.core.windows.net/hostingsvcprod/config.json";
-
-  const IbizaProdStages = ["stage1", "stage2", "stage3", "stage4", "stage5"];
-  const IbizaProdToRegion = {
-  stage1: "Central US EUAP",
-  stage2: "West Central US",
-  stage3: "South Central US",
-  stage4: "West US",
-  stage5: "Remaining Regions"
-  };
-  const timeStamp = new Date().toISOString();
-  const versionFileCall = await axios.get(ibizaVersionsUri);
-  const versionFile = Object.keys(versionFileCall.data)
-    .filter(x => !x.includes("$"))
-    .map(x => ({
-      name: x,
-      version: versionFileCall.data[x],
-      timeStamp
-    }));
-  const db = await MongoClient.connect(url);
-  try {
-    const dbo = db.db("versions");
-    const p = versionFile.map(async v => {
-      var query = { name: v.name };
-      const lastInsertedVersion = await dbo
-        .collection("ibiza")
-        .find(query)
-        .limit(1)
-        .sort({ timeStamp: -1 })
-        .toArray();
-
-      if (
-        lastInsertedVersion.length === 0 ||
-        lastInsertedVersion[0].version !== v.version
-      ) {
-
-        const t = await dbo.collection("ibiza").insertOne(v);
-        if(v.name.includes('stage')){
-        const t = await axios({
-          method: 'post',
-          url: process.env.EventWebhookUrl,
-          data: {
-            environment:IbizaProdStages.includes(v.name) ? "prod" : "stage",
-            portal: "ibiza",
-            oldVersion: lastInsertedVersion.length === 0 ? '' : lastInsertedVersion[0].version,
-            newVersion: v.version,
-            regions: IbizaProdToRegion[v.name]
-          }
-        })
-      }
-        context.log("inserted a new version");
-      } else {
-        context.log("version already exists");
-      }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-    await Promise.all(p);
-    db.close();
-  } catch (err) {
-    context.log(err);
-    db.close();
-  }
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var typeorm_1 = require("typeorm");
+var ibiza_version_entity_1 = require("./entities/ibiza-version.entity");
+var axios_1 = require("axios");
+function run(context, myTimer) {
+    return __awaiter(this, void 0, void 0, function () {
+        var ibizaVersionsUri, connection, IbizaProdStages, IbizaProdToRegion, versionFileCall, versionFile, p;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    ibizaVersionsUri = 'https://appsvcstorage.blob.core.windows.net/hostingsvcprod/config.json';
+                    return [4 /*yield*/, typeorm_1.createConnection({
+                            type: 'postgres',
+                            host: process.env.POSTGRES_ENDPOINT,
+                            port: 5432,
+                            username: process.env.POSTGRES_USERNAME,
+                            password: process.env.POSTGRES_PASSWORD,
+                            database: process.env.POSTGRES_DB,
+                            ssl: true,
+                            name: 'ibiza',
+                            entities: [ibiza_version_entity_1.IbizaVersion]
+                        })];
+                case 1:
+                    connection = _a.sent();
+                    IbizaProdStages = ['stage1', 'stage2', 'stage3', 'stage4', 'stage5'];
+                    IbizaProdToRegion = {
+                        stage1: 'Central US EUAP',
+                        stage2: 'West Central US',
+                        stage3: 'South Central US',
+                        stage4: 'West US',
+                        stage5: 'Remaining Regions'
+                    };
+                    return [4 /*yield*/, axios_1["default"].get(ibizaVersionsUri)];
+                case 2:
+                    versionFileCall = _a.sent();
+                    versionFile = Object.keys(versionFileCall.data)
+                        .filter(function (x) { return x.indexOf('$') === -1; })
+                        .map(function (x) { return ({
+                        name: x,
+                        version: versionFileCall.data[x]
+                    }); });
+                    p = versionFile.map(function (v) { return __awaiter(_this, void 0, void 0, function () {
+                        var lastVersion, post;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, connection.manager
+                                        .getRepository(ibiza_version_entity_1.IbizaVersion)
+                                        .createQueryBuilder('version')
+                                        .where('version.name = :name', { name: v.name })
+                                        .orderBy('version.createdAt', 'DESC', 'NULLS LAST')
+                                        .getOne()];
+                                case 1:
+                                    lastVersion = _a.sent();
+                                    console.log(lastVersion);
+                                    console.log(v);
+                                    if (!(!lastVersion || lastVersion.version !== v.version)) return [3 /*break*/, 3];
+                                    console.log('updated');
+                                    post = connection.manager.create(ibiza_version_entity_1.IbizaVersion, v);
+                                    return [4 /*yield*/, connection.manager.save(ibiza_version_entity_1.IbizaVersion, post)];
+                                case 2:
+                                    _a.sent();
+                                    _a.label = 3;
+                                case 3: return [2 /*return*/];
+                            }
+                        });
+                    }); });
+                    return [4 /*yield*/, Promise.all(p)];
+                case 3:
+                    _a.sent();
+                    connection.close();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.run = run;
+//# sourceMappingURL=index.js.map
